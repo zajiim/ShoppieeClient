9cefaaeef208df26f442f52d6b818f6ac94c90b7
Zajiim <sajims95@gmail.com>
2024-10-01 16:40:09 +0530

    Implemented animations for navigation

--- app/src/main/java/com/example/shoppieeclient/presentation/navigation/ShoppieNavGraph.kt
+++ app/src/main/java/com/example/shoppieeclient/presentation/navigation/ShoppieNavGraph.kt
@@ -1,5 +1,11 @@
 package com.example.shoppieeclient.presentation.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -25,7 +31,19 @@ fun ShoppieNavGraph(
     val startDestination by mainActivityViewModel.startDestination.collectAsState()
     NavHost(
         navController = navController,
-        startDestination = startDestination
+        startDestination = startDestination,
+        enterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left, animationSpec = tween(600))
+        },
+        exitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left, animationSpec = tween(600))
+        },
+        popEnterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Right, animationSpec = tween(600))
+        },
+        popExitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right, animationSpec = tween(600))
+        }
     ) {
         composable<Destination.Onboarding> {
             val onBoardingViewModel: OnBoardingViewModel = koinViewModel()
@@ -50,7 +68,9 @@ fun ShoppieNavGraph(
         composable<Destination.SignUp> {
             SignUpScreen(
                 onSignInClicked = {
+//                    navController.navigateUp()
                     navController.navigate(Destination.SignIn) {
+                        launchSingleTop = true
                         popUpTo(Destination.SignUp) {inclusive = true}
                     }
                 },
